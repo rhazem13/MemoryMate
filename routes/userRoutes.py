e8211a32efd711fc44a89262aad2eed6c55ecca5
shraf <eldeenshraf@gmail.com>
2022-12-11 20:14:19 +0200

    pycache

--- routes/userRoutes.py
+++ routes/userRoutes.py
@@ -1,18 +1,81 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, reqparse, abort
-from models.user.userModel import User
+from repositories import UserRepo
+from models.user import userModel
+from models.db import db
+
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
 
 
-class UserRouter(Resource):
+user_put_args = reqparse.RequestParser()
+user_put_args.add_argument("id", type=int, help="id")
+
+#user_put_args.add_argument("public_id", type=int, help="id")
+
+user_put_args.add_argument("username", type=str, help="username")
+user_put_args.add_argument("password", type=str, help="password")
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 401
+
+        try: 
+            data = jwt.decode(token,"'secret'")
+            current_user = UserRepo.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
+class AdminRegisterRouter(Resource):
+    @token_required
+    def get(current_user):
+        
+        # if not current_user.admin:
+        #return jsonify({'message' : 'Cannot perform that function!'})
+
+       
+        return UserRepo.get_all()
+
+    def post(current_user):
+      
+        args = user_put_args.parse_args()
+        hashed_password = generate_password_hash(args['password'], method='sha256')
+        new_user = UserRepo(public_id=str(uuid.uuid4()), name=args['name'], password=hashed_password, admin=False)
+        db.session.add(new_user)
+        db.session.commit()
     
-    def get(self):
-        #get user data by id
-        return "shaaf"
+    def login():
+       auth = request.authorization
 
-    def patch(self):
-        #modify user data by id
-        return 1
-    
-    def delete(self):
-        #delete user by id
-        return 1
+       if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+       user = UserRepo.query.filter_by(name=auth.username).first()
+
+       if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+       if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},'secret')
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+       return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+
