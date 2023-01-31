81dd758fd7a20843c46c780d3c8732550c79c135
shraf <eldeenshraf@gmail.com>
2023-01-31 13:13:41 +0200

    setup caching with redis

--- app.py
+++ app.py
@@ -2,23 +2,28 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request,jsonify,make_response,session
 from flask_restful import Api
 from flask_socketio import SocketIO
+from flask_migrate import Migrate
+from flask_caching import Cache
+from services.caching import CacheService
 import os
 from dotenv import load_dotenv
 from models.db import db
-from routes.userRoutes import user_bp
-from flask_migrate import Migrate
+
 
 load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
 app.config.from_object('config.Config')  # Set the configuration variables to the flask application
-app.register_blueprint(user_bp, url_prefix='/users')
 migrate= Migrate(app,db)
 migrate.init_app(app, db)
-
 socketio  = SocketIO(app, cors_allowed_origins='*')
 db.init_app(app)
+CacheService.initialize(app)
+from routes.userRoutes import UserRouters
+userRouter =  UserRouters()
+app.register_blueprint(userRouter.user_bp, url_prefix='/users')
+
 @socketio.on('connect')
 def test_connect():
     print('sharaf')
@@ -27,6 +32,7 @@ def test_connect():
 #     db.create_all()
 
 #if __name__ == "main":
+
 app.run(debug = True)
 
 
--- routes/userRoutes.py
+++ routes/userRoutes.py
@@ -1,52 +1,50 @@
 from flask import Flask, request, Blueprint
 from flask_restful import Resource, reqparse, abort
 from flask_bcrypt import generate_password_hash
-from models.User.userModel import User
+from models.user.userModel import User
 from repositories import UserRepo
 import jwt
 import datetime
 from flask import Response
 import json
-user_bp = Blueprint('users', __name__)
+import random
+from services.caching import CacheService
+from flask_caching import Cache
 
-""" user_put_args = reqparse.RequestParser()
+class UserRouters:
+    user_bp = Blueprint('users', __name__)
+    cache = CacheService.get_instance()
+    @user_bp.get('/hello')
+    @cache.cached(key_prefix='sharaf')
+    def get():
+        #get user data by id
+        n = random.randint(0,10000)
+        print('holaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        return str(n)
 
-user_put_args.add_argument("username", type=str, help="email")
-user_put_args.add_argument("password", type=str, help="password")
-user_put_args.add_argument("password", type=str, help="firstname")
-user_put_args.add_argument("password", type=str, help="lastname")
-user_put_args.add_argument("password", type=str, help="date_of_birth")
-user_put_args.add_argument("password", type=str, help="address")
-user_put_args.add_argument("password", type=str, help="type")
- """
-@user_bp.get('/hello')
-def get():
-    #get user data by id
-    return "shaaf"
+    @user_bp.post('/register')
+    def register():
+        payload =request.get_json()['user']
+        hashed_password = generate_password_hash(payload['password'], 10)
+        payload['password'] = hashed_password
+        user = UserRepo.create(payload)
+        token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},'secret')
+        return {'token': token}
 
-@user_bp.post('/register')
-def register():
-    payload =request.get_json()['user']
-    hashed_password = generate_password_hash(payload['password'], 10)
-    payload['password'] = hashed_password
-    user = UserRepo.create(payload)
-    token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},'secret')
-    return {'token': token}
+    @user_bp.post('/login')
+    def login():
+        payload = request.get_json()['user']
+        user = UserRepo.get_by_email(payload['email'])
+        if  user is None:
+            return Response(status = 404)
+        password = user.hashed_password
+        if password == payload['password']:
+            return Response(json.dumps({'message':'success'}), status = 200, mimetype='application/json')
+        return Response(status=403)
 
-@user_bp.post('/login')
-def login():
-    payload = request.get_json()['user']
-    user = UserRepo.get_by_email(payload['email'])
-    if  user is None:
-        return Response(status = 404)
-    password = user.hashed_password
-    if password == payload['password']:
-        return Response(json.dumps({'message':'success'}), status = 200, mimetype='application/json')
-    return Response(status=403)
+    #@user_bp.get("/me") / it returns the information of the current user based on the token
 
-#@user_bp.get("/me") / it returns the information of the current user based on the token
+    #@user_bp.get('/locations') it returns the locations of the current user
 
-#@user_bp.get('/locations') it returns the locations of the current user
-
-#@user_bp.post('/locations') it adds a location for a user
-#@user_bp.put('/locations/{location_id}') it updates a location by it's id
+    #@user_bp.post('/locations') it adds a location for a user
+    #@user_bp.put('/locations/{location_id}') it updates a location by it's id
