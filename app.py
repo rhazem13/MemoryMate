a587f29144ed2aafb88441c2ddc54580def80c47
shraf <eldeenshraf@gmail.com>
2023-02-03 15:10:10 +0200

    creating event emitter

--- app.py
+++ app.py
@@ -3,17 +3,15 @@ from flask import Flask, request,jsonify,make_response,session
 from flask_restful import Api
 from flask_socketio import SocketIO
 from flask_migrate import Migrate
-from flask_caching import Cache
 from services.caching.caching import CacheService
-import os
 from dotenv import load_dotenv
 from models.db import db
-from routes.userRoutes import user_bp
 from routes.userLocationRoutes import user_location_bp
 from routes.userAgendaRoutes import user_agenda_bp
 from routes.userCalendarRoutes import user_calendar_bp
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from services.EventEmitter.event_emitter import EventEmitter
 
 
 load_dotenv()
@@ -34,18 +32,22 @@ app.register_blueprint(user_location_bp, url_prefix='/userlocation')
 app.register_blueprint(user_agenda_bp, url_prefix='/useragenda')
 app.register_blueprint(user_calendar_bp, url_prefix='/usercalendar')
 socketio  = SocketIO(app, cors_allowed_origins='*')
-
+emitter = EventEmitter.getInstance()
+def test_event(keyword, name):
+    print('test socket in function ')
+    print(keyword, name)
+print('event test has gone')
 @socketio.on('connect')
 def test_connect():
     print('sharaf')
+    emitter.on('an-event', test_event)
     socketio.emit('after connect', {'data':'Let us learn Web Socket in Flask'})
 
 # with app.app_context():
 #     db.create_all()
 
-if __name__ == "main":
 
-    #app.run(debug = True)
-    print('starting socket')
-    socketio.run(app, debug = True, host='127.0.0.1')
+app.run(debug = True)
+print('starting socket')
+#socketio.run(app, debug = True, host='127.0.0.1')
 
