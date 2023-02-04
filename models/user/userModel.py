95350f76684515f19d896b450515f7984a7b7a06
shraf <eldeenshraf@gmail.com>
2023-02-04 14:29:09 +0200

    test rename dicrectory of user model

--- models/user/userModel.py
+++ models/user/userModel.py
@@ -9,7 +9,7 @@ from models.Notifications import notificationsModel
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False) 
     firstname = db.Column(db.String(100), nullable=False)
     lastname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
