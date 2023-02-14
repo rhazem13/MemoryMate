830a64b8cb0887443fe93d4e98bf8fc283a92ace
rhazem13 <rhazem13@yahoo.com>
2023-02-14 18:07:58 +0200

    add getting current close friends,waypoints

--- middlewares/validation/userValidation.py
+++ middlewares/validation/userValidation.py
@@ -2,6 +2,7 @@ from marshmallow.validate import Length,OneOf,Regexp,URL
 from marshmallow import Schema, fields, validates, ValidationError
 import datetime
 import werkzeug
+import json
 
 
 
@@ -11,11 +12,13 @@ pass_regex=r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
 #starts with +country code
 phone_regex=r"^\+[1-9]{1}[0-9]{3,14}$"
 
-
 class CreateUserscheme(Schema):
-    username = fields.Str(required=True, validate=Length(min=3,max=60))
+    class Meta:
+        fields = ("username","firstname","lastname","location","email","password","file","user_type","address","phone","date_of_birth")
+    username = fields.Str(required=True, validate=Length(min=3,max=60)) 
     firstname=fields.Str(required=True,validate=Length(min=3,max=60))
     lastname=fields.Str(required=True,validate=Length(min=3,max=60))
+    location = fields.Method("get_location", deserialize="load_location")
     email=fields.Email(required=True)
     password=fields.Str(required=True,validate=Regexp(pass_regex))
     file =fields.Raw(type=werkzeug.datastructures.FileStorage) 
@@ -29,6 +32,14 @@ class CreateUserscheme(Schema):
         now = datetime.datetime.now().date()
         if value > now:
             raise ValidationError("Can't be born in the future!")
+
+    def get_location(self, obj):
+        if(obj.location==None):
+            return None
+        return json.loads(obj.location)
+
+    def load_location(self, obj):
+        return obj
 
 class LoginUserscheme(Schema):
     email=fields.Email(required=True)
