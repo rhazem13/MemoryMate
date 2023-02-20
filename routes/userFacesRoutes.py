2d0ed7c56b471b2c832df0cafc485b564ffb9281
shraf <eldeenshraf@gmail.com>
2023-02-20 23:28:43 +0200

    add alaa function

--- routes/userFacesRoutes.py
+++ routes/userFacesRoutes.py
@@ -20,6 +20,43 @@ def post():
         return "Id field shouldn't be entered",422
     return singleSchema.dump(facesRepository.create(payload))
 
+""" def post():
+    errors= singleSchema.validate(request.form)
+    if errors:
+        return errors, 422
+    
+
+    payload =FacesSchema().load(request.form)
+    print("found")
+
+    if('id' in payload):
+        return "Id field shouldn't be entered",422
+    
+    if 'user_face' not in request.files:
+        resp=jsonify({'message' : 'No  image  in the request'})
+        resp.status_code=400
+        return resp
+    user_face = request.files['user_face']
+    if user_face.filename=='':
+        resp=jsonify({'message' : 'No image selected for uploading'})
+        resp.status_code=400
+        return resp
+    if user_face and allowed_file(user_face.filename):
+         
+     user_face_name = secure_filename(user_face.filename)
+     user_face_path =  "static/faces/" + user_face_name
+     user_face.save(user_face_path)
+
+     payload['']=user_face_path #######add column name here
+     resp=jsonify({'message' : 'face uploaded suecessfully'})
+     resp.status_code=201
+     return singleSchema.dump(faceRepository.create(payload))
+
+    else:
+     resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+     resp.status_code=400
+     return resp """
+
 @user_face_bp.get('')
 @token_required
 def get():
