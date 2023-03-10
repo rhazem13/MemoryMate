95caa65cff99c374b667b5b74055c5bc33320a80
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-03-10 14:22:44 +0200

    UpdateBase64

--- routes/FaceRecognationRoutes.py
+++ routes/FaceRecognationRoutes.py
@@ -6,7 +6,7 @@ import os
 from flask import  request, jsonify, Blueprint
 import cv2
 import numpy as np
-
+from MachineLearning.test import readb64
 
 
 FaceRecognation = Blueprint('Face', __name__)
@@ -86,6 +86,73 @@ def Recognation():
          pic.save(img_path)
 
          predict_result = TestFaces(img_path)
+
+       
+         
+         return predict_result
+    
+
+
+@FaceRecognation.route('/RecBase64', methods=['POST'])
+def Recognation():
+    def TestFaces(test_image):
+        path = "MachineLearning/Face_Recognation/train/"
+
+        known_names = []
+        known_name_encodings = []
+        images = os.listdir(path)
+        for _ in images:
+                image = fr.load_image_file(path + _)
+                image_path = path + _
+                encoding = fr.face_encodings(image)[0]
+                known_name_encodings.append(encoding)
+                known_names.append(os.path.splitext(os.path.basename(image_path))[0].capitalize())
+
+                
+       
+        image = cv2.imread(test_image)
+        
+
+        face_locations = fr.face_locations(image)
+        face_encodings = fr.face_encodings(image, face_locations)
+
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            matches = fr.compare_faces(known_name_encodings, face_encoding)
+            name = "Unknown"
+
+            face_distances = fr.face_distance(known_name_encodings, face_encoding)
+            best_match = np.argmin(face_distances)
+
+            if matches[best_match]:
+                name = known_names[best_match]
+            
+
+
+            cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(image, (left, bottom - 15), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+     
+        # cv2.imwrite("MachineLearning/Face_Recognation/output.jpg", image)
+       
+
+        return {"The Person is": name}
+    
+    if  request.method == 'POST':
+         
+         if 'ph' not in request.json:
+            resp = jsonify({'message':'No file part in the request'})
+            resp.status_code=400
+            return resp
+         pic =request.json['ph']
+        #  img_path =  "Faces/Tests/" + pic.filename
+          
+        #  pic.save(img_path)
+        
+         img = readb64(pic)
+
+         predict_result = TestFaces(img)
 
        
          
